c170a8507ddd894dcf30f4067b46d0b3c52a4002
Gaurav Tenkila <tsgaurav@gmail.com>
2023-03-13 16:49:25 -0700

    Modified percolation transition check for openBC

--- super_script_bdry.py
+++ super_script_bdry.py
@@ -5,7 +5,7 @@ import time
 
 #os.system("pwd")
 for L in [30]:
-    for w_blk in [2.6]:#np.arange(1, 3.5, 0.2):
+    for w_blk in np.arange(3.6, 3.8, 0.2):
         for w_bdry in [2.6]:#np.arange(0.01, 0.05, 0.01):
             for w_mixed in [2.6]:
                 os.system("nq -c mpirun run_script_bdry.py "+str(L)+" "+str(w_blk)+" "+str(w_bdry)+" "+str(w_mixed))

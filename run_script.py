6b25f216d6ccedfe064641ace5c148c3eba6a770
Gaurav Somashekar Tenkila <tsgaurav@login02.sockeye.arc.ubc.ca>
2023-05-03 02:11:24 -0700

    Merge fixes

--- run_script.py
+++ run_script.py
@@ -27,13 +27,12 @@ measure_list = gen_check_list(L*L, steps-1, 20)
 
 #cluster_dict_list = [np.array([]) for step in range(len(measure_list))]
 
-n_runs = 5
+n_runs = 30
 
 input_dict = {"L":L, "steps":steps,"measure_list":measure_list,'a':a, 'b':b,'w':w, "n_runs":n_runs*n_processes}
 
 
 if rank == 0: # The master is the only process that reads the file
-    #data = data# something read from file
 	ts = str(int(time.time()))
 	data = [[ts]]*n_processes
 	with open("output/Ising_2D_output_"+ts+".txt", "w") as writer:
@@ -60,38 +59,29 @@ reverse_clust_dict_list = []
 
 for item in data:  #Sending to processes
 	for inst in range(n_runs):  #Within each process
-        
-        #J_ij_vals = fill_J_ij_matrix(L*L, nn_ind, nnn_ind, a, b)
 		J_ij_vals = fill_J_ij_matrix_width(L*L, nn_ind, a, b, w)
 		h_vals = np.exp(-np.random.exponential(size=L*L))
 		test = system(L*L, deepcopy(nn_ind), J_ij_vals, h_vals)
 		check_acc = 0
 		for i in range(steps):
 			test.decimate()
-			if i in measure_list: 
+			if i in measure_list:
 				h_remain = test.h_vals[test.h_vals!=0]
-				#h_dist_list[check_acc] = np.concatenate((h_dist_list[check_acc],-np.log(h_remain/test.Omega)))
-
-				J_remain = -np.log(sparse.find(test.J_ij_vals)[2]) + np.log(test.Omega)
-				#J_dist_list[check_acc] = np.concatenate((J_dist_list[check_acc], J_remain))
 				with open("output/Ising_2D_output_"+item+".txt", "a") as writer:
 					i_num = f"{rank:02}" + f"{inst:02}"
-                    			writer.write("In"+i_num+"_h_m"+f"{check_acc:02}")
-                    			json.dump((-np.log(h_remain/test.Omega)).tolist(), writer)
-                    			writer.write('\n')
+					writer.write("In"+i_num+"_h_m"+f"{check_acc:02}")
+					json.dump((-np.log(h_remain/test.Omega)).tolist(), writer)
+					writer.write('\n')
 				check_acc+=1
-				#Omega_list_composite = np.concatenate((Omega_list_composite, np.array(test.Omega_array)))
-				#decimation_type_composite = np.concatenate((decimation_type_composite, np.array(test.coupling_dec_list, dtype=bool)))
-		cluster_dict_list.append(test.clust_dict)
-		reverse_clust_dict_list.append(test.reverse_dict)
-
+			cluster_dict_list.append(test.clust_dict)
+			reverse_clust_dict_list.append(test.reverse_dict)
 #data = (J_dist_list, h_dist_list, Omega_list_composite, decimation_type_composite)
 clust_data = [cluster_dict_list, reverse_clust_dict_list]
 
 # Send the results back to the master processes
 
 
-processed_data = comm.gather(data,root=0)
+#processed_data = comm.gather(data,root=0)
 clust_list_final = comm.gather(clust_data, root=0)
 
 

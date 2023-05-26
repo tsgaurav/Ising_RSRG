31638ef7ec67f231d9bb8931869bd6761c4c6353
Gaurav Tenkila <tsgaurav@gmail.com>
2023-05-25 17:07:34 -0700

    Log decimation optimizations

--- RSRG_log_class.py
+++ RSRG_log_class.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 
 class log_system:
     
-    def __init__(self, size, adj_ind, zeta_ij_vals, beta_vals):
+    def __init__(self, size, adj_ind, zeta_ij_vals, beta_vals, track_moments = False):
         
         self.size = size
         self.adj_ind = adj_ind
@@ -19,10 +19,14 @@ class log_system:
         self.clust_dict = {i:i for i in range(size)}
         self.reverse_dict = {i:[i] for i in range(size)}
         
+        self.track_moments = track_moments
+        self.moment_list = [1.0]
+
+
         return None
     
     def decimate(self):
-        beta_min, zeta_min = self.beta_vals[self.beta_vals>0].min(), sparse.find(self.zeta_ij_vals)[2].min()
+        beta_min, zeta_min = self.beta_vals[self.beta_vals>0].min(), self.zeta_ij_vals.data.min()
         Gamma = min(beta_min, zeta_min)
         
         if Gamma == zeta_min: self.zeta_decimation(Gamma)
@@ -34,6 +38,8 @@ class log_system:
         self.Gamma = Gamma
         self.Gamma_array.append(Gamma)
         
+        if self.track_moments: self.moment_list.append(self.get_moment())
+
         return None
     
     def zeta_decimation(self, Gamma):
@@ -41,27 +47,35 @@ class log_system:
         r_ind, c_ind, zeta_ij = sparse.find(self.zeta_ij_vals) 
         zeta_ind = np.where(zeta_ij == Gamma)[0][0]
         i, j = r_ind[zeta_ind], c_ind[zeta_ind]
-        print
+
         self.clust_dict, self.reverse_dict = update_cluster(self.clust_dict, self.reverse_dict, i, j)
 
-        self.beta_vals[i] = self.beta_vals[i] + self.beta_vals[j]    
+        self.beta_vals[i] +=  self.beta_vals[j]    
         self.beta_vals[j] = 0
 
         self.adj_ind = update_adjacency_zeta_ij(self.adj_ind, i, j)
 
-        self.zeta_ij_vals[i,self.adj_ind[i]] = \
-        (self.zeta_ij_vals[i, self.adj_ind[i]] + self.zeta_ij_vals[j, self.adj_ind[i]])/2
+        self.zeta_ij_vals[i,self.adj_ind[i]] += self.zeta_ij_vals[j, self.adj_ind[i]]
+        self.zeta_ij_vals[i,self.adj_ind[i]] /= 2
         
         
         self.zeta_ij_vals[self.adj_ind[i], i] = self.zeta_ij_vals[i,self.adj_ind[i]]
 
-        eye = chunk_deleter([j], self.size)
-        self.zeta_ij_vals = eye.dot(self.zeta_ij_vals).dot(eye)
+        #eye = chunk_deleter([j], self.size)
+        #self.zeta_ij_vals = eye.dot(self.zeta_ij_vals).dot(eye)
+        
+        # Set the specified row to zero
+        self.zeta_ij_vals.data[self.zeta_ij_vals.indptr[j]:self.zeta_ij_vals.indptr[j +1]] = 0
+
+        # Set the specified column to zero
+        bool_arr = self.zeta_ij_vals.indices == j
+        self.zeta_ij_vals.data[bool_arr] = 0
+        
+        self.zeta_ij_vals.eliminate_zeros()
         
         return None
     
     def beta_decimation(self, Gamma):
-        #print("beta")
         i = np.where(self.beta_vals == Gamma)[0][0]
         adj_i = self.adj_ind[i]
         
@@ -73,29 +87,39 @@ class log_system:
         zeta_subblock[np.where(zeta_subblock==0)]=1000
         new_couplings = np.minimum(zeta_subblock, new_couplings)
         self.zeta_ij_vals[np.ix_(adj_i, adj_i)] = new_couplings
-        """
         
+        
+        """
         ### GPTest
         
-        zeta_subblock = self.zeta_ij_vals[np.ix_(adj_i, adj_i)].toarray().astype(float)
+        zeta_subblock = self.zeta_ij_vals[adj_i, :][:, adj_i].toarray()
 
         old_couplings = self.zeta_ij_vals[adj_i, i].data
 
         new_couplings = np.add.outer(old_couplings, old_couplings)
         np.fill_diagonal(new_couplings, 0)
 
-        mask = zeta_subblock == 0
-        zeta_subblock[mask] = 1000
-
+        zeta_subblock[np.where(zeta_subblock == 0)] = 1000
+        
         np.minimum(zeta_subblock, new_couplings, out=zeta_subblock)
 
         self.zeta_ij_vals[np.ix_(adj_i, adj_i)] = sparse.csr_matrix(zeta_subblock)
+        
+        
+        #eye = chunk_deleter([i], self.size)
+        #self.zeta_ij_vals = eye.dot(self.zeta_ij_vals).dot(eye)
+        
+        # Set the specified row to zero
+        self.zeta_ij_vals.data[self.zeta_ij_vals.indptr[i]:self.zeta_ij_vals.indptr[i +1]] = 0
 
-
-        eye = chunk_deleter([i], self.size)
-        self.zeta_ij_vals = eye.dot(self.zeta_ij_vals).dot(eye)
-
+        # Set the specified column to zero
+        bool_arr = self.zeta_ij_vals.indices == i
+        self.zeta_ij_vals.data[bool_arr] = 0
+        
+        self.zeta_ij_vals.eliminate_zeros()
+        
         self.adj_ind = update_adjacency_beta(self.adj_ind, i)
+        
         return None
         
         
